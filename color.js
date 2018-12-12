f7aa09fa5ce307b19041a1a6897782269673438a
Phil Szalay <philszalay@dhcp-172-21-85-101.wlan.uni-hamburg.de.local>
2018-12-12 10:57:18 +0100

    calc freqs for low, mid, high boxes

--- color.js
+++ color.js
@@ -4,41 +4,23 @@ var colorBox_mid = document.getElementById('colorBox_mid');
 var colorBox_low = document.getElementById('colorBox_low');
 var audio, volume, frequencies, frequency, audioContext, analyser, 
     microphone, waveform, amplitude;
-var outputHigh = 3000;
-var inputLow = 100;
 
 var subBassLimit = 60;
 var bassLimit = 250;
 var midLimit = 4000;
 var highLimit = 20000;
 
-function sig2hz (frequencies, options) {
-    var rate = 22050 / 1024; // defaults in audioContext.
-  
-    if (options) {
-      if (options.rate) {
-        rate = options.rate;
-      }
+function freqDataToBoxFreqs (frequencyData) {
+    return {
+        low: (frequencyData[0] + frequencyData[1]) / 2,
+        mid: (frequencyData[2] + frequencyData[3] + frequencyData[4]) / 3,
+        high: (frequencyData[5] + frequencyData[6] + frequencyData[7]) / 3,
     }
-  
-    var maxI, max = frequencies[0];
-    
-    for (var i=0; frequencies.length > i; i++) {
-      var oldmax = parseFloat(max);
-      var newmax = Math.max(max, frequencies[i]);
-      if (oldmax != newmax) {
-        max = newmax;
-        maxI = i;
-      } 
-    }
-    return maxI * rate;
 }
 
-function random_bg_color(frequency) {
+function random_bg_color(frequency, box) {
     var r = Math.floor(Math.random() * 256);
-    x = y = z = ((frequency -inputLow) / (outputHigh) * 255); 
-    console.log(x);
-    return "rgb(" + (x) + "," + (y) + "," + (z) + ")";  
+    return "rgb(" + (frequency) + "," + (frequency) + "," + (frequency) + ")";  
 }
     
 navigator.getUserMedia(
@@ -48,31 +30,25 @@ navigator.getUserMedia(
     }, function (stream) {
         audioContext = new AudioContext();
         analyser = audioContext.createAnalyser();
-        frequencies = new Float32Array(analyser.frequencyBinCount);
-        amplitude = new Uint8Array(analyser.frequencyBinCount);
-        
-        volume = audioContext.createGain();
-        
-        microphone = audioContext.createMediaStreamSource(stream);
-        microphone.connect(volume);
-        microphone.connect(analyser);
+        source = audioContext.createMediaStreamSource(stream);
+        source.connect(analyser);
+        analyser.fftSize = 32;
+        frequencyData = new Uint8Array(analyser.frequencyBinCount);
 
         renderFrame();
     }, function (error) { console.log(error) });
 
 function renderFrame() {
-    lastFrequency = frequency;
-    requestAnimationFrame(renderFrame);
-    analyser.getFloatFrequencyData(frequencies);
-    analyser.getByteTimeDomainData(amplitude);
+    analyser.getByteFrequencyData(frequencyData);
+
+    frequencies ? oldFrequencies = frequencies : null;
+    frequencies = freqDataToBoxFreqs(frequencyData);
     
-    frequency = sig2hz(frequencies);
+    console.log(frequencies);
 
-    // console.log(frequency);
-
-    if (frequency !== lastFrequency) {
-        frequency < bassLimit ? colorBox_low.style.backgroundColor = random_bg_color(frequency) : null;
-        frequency >= bassLimit && frequency < midLimit ? colorBox_mid.style.backgroundColor = random_bg_color(frequency): null;
-        frequency >= midLimit && frequency <= highLimit ? colorBox_high.style.backgroundColor = random_bg_color(frequency): null;
-    }
+    colorBox_low.style.backgroundColor = random_bg_color(frequencies.low, 'low');
+    colorBox_mid.style.backgroundColor = random_bg_color(frequencies.mid, 'mid');
+    colorBox_high.style.backgroundColor = random_bg_color(frequencies.high, 'high');
+    
+    requestAnimationFrame(renderFrame);
 }
